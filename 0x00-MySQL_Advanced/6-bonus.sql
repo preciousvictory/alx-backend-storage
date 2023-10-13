c4f79c7bae5f021643c59cd4c7bc9dec5d9388a2
Victory Abiodun-Omoniyi <victoryexcel2018@gmail.com>
2023-10-12 19:42:36 -0700

    create procedure declare if insert

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,23 @@
+delimiter |
+
+CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name varchar(255), IN score INT)
+BEGIN
+    DECLARE project_id INT DEFAULT 0;
+    DECLARE p_id INT DEFAULT 0;
+
+    SELECT COUNT(id)
+        INTO project_id 
+        FROM projects
+        WHERE name = project_name
+	LIMIT 1;
+    IF project_id = 0 THEN
+        INSERT INTO projects (name) VALUES (project_name);
+    END IF;
+
+    SELECT id
+        INTO p_id
+        FROM projects
+        WHERE name = project_name;
+    INSERT INTO corrections(user_id, project_id, score) VALUES (user_id, p_id, score);
+END
+|
