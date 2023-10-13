017910729b312da0bca54463790de57a8a32934e
Victory Abiodun-Omoniyi <victoryexcel2018@gmail.com>
2023-10-12 19:43:40 -0700

    create procedure declare if insert

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,3 +1,4 @@
+-- creates a stored procedure AddBonus that adds a new correction for a student.
 delimiter |
 
 CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name varchar(255), IN score INT)
