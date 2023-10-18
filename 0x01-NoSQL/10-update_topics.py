0065a9f87d692007b71cd852ed694ee1847de197
Victory Abiodun-Omoniyi <victoryexcel2018@gmail.com>
2023-10-17 18:25:08 -0700

    update document in collection pymongo

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -7,4 +7,4 @@ from pymongo import MongoClient
 def update_topics(mongo_collection, name, topics):
     """update_topics
     """
-    mongo_collection.update_one({"name": name}, {"$set": {"topics": topics}});
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" 11-schools_by_topic
+"""
+from pymongo import MongoClient
+
+
+def schools_by_topic(mongo_collection, topic):
+    """mongo_collection
+    """
+    l
