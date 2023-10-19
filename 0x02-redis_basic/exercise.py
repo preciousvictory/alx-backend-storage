c39a3f97668e73c7adfcd5c96ccf4687bf447830
Victory Abiodun-Omoniyi <victoryexcel2018@gmail.com>
2023-10-18 19:32:33 -0700

    redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@ exercise file
 """
 import redis
 import uuid
-from typing import Any
+from typing import Any, Optional, Callable, Union
 
 
 class Cache():
@@ -14,9 +14,27 @@ class Cache():
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Any) -> str:
+    def store(self, data: Union[str, int, bytes, float]) -> str:
         """store method
         """
-        key = str(uuid.uuid1())
+        key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Any:
+        """get data
+        """
+        value = self._redis.get(key)
+        if fn is int:
+            return self.get_int(value)
+        elif fn is str:
+            return self.get_str(value)
+        return value
+
+    def get_str(self, value: bytes) -> str:
+        """ get string"""
+        return str(value)
+
+    def get_int(self, value: bytes) -> int:
+        """"get number"""
+        return int(value)
